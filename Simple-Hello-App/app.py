33b63eb1c854b04c55c3dbe6b2b0f371d6913262
arnabmma77 <159787525+arnabmma77@users.noreply.github.com>
2025-03-31 04:13:21 +0530

    Add files via upload

---
+++ Simple-Hello-App/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title('Hello World!')
+
+st.write('This is a simple Streamlit app by Arnab Mukherjee')
+
+st.balloons()       # Celebration mode 🎈
+
+st.snow()           # Winter wonderland ❄️
+
+st.progress(0.5)    # Show progress 🚧
